85de5b1a18917a10ba1f02cdd24a887af801f76c
TN1608 <120995414+TN1608@users.noreply.github.com>
2024-11-07 15:23:16 +0700

    First

---
+++ src/test/java/java6/com/Java6AsmApplicationTests.java
@@ -0,0 +1,13 @@
+package java6.com;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Java6AsmApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
